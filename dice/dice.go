4f025b1ef3e721f42974cb642ebf843a3dbf90d7
Ben Miller <ben@beepmill.com>
2018-08-11 21:45:33 -0600

    now, with rolling!

--- dice/dice.go
+++ dice/dice.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"regexp"
 	"flag"
 	"fmt"
+	"math/rand"
+	"regexp"
+	"strconv"
+	"time"
 )
 
 var (
@@ -12,16 +15,36 @@ var (
 
 // Roll TODO
 func Roll(command string) error {
-	fmt.Printf("Rolling %v", command)
+	rand.Seed(time.Now().UTC().UnixNano())
+	rollValid := regexp.MustCompile(`(\d+)[dD](\d+)`)
+	match := rollValid.FindStringSubmatch(command)
+	fmt.Printf("Rolling %v (%s rolls of size %s)\n", command, match[1], match[2])
+	result := 0
+	rolls, err := strconv.Atoi(match[1])
+	size, err := strconv.Atoi(match[2])
+
+	if err != nil {
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	for r := 0; r < rolls; r++ {
+		roll := rand.Intn(size) + 1
+		result += roll
+	}
+	fmt.Printf("%v", result)
 	return nil
 }
 
 func main() {
 	var roll = flag.String("roll", "1d6", "help message goes here")
 	flag.Parse()
-	
-	rollValid := regexp.MustCompile("\\d+[dD]\\d+")
-	rollValid.MatchString(*roll)
 
-	Roll(*roll)
+	rollValid := regexp.MustCompile(`\d+[dD]\d+`)
+	if rollValid.MatchString(*roll) == true {
+		Roll(*roll)
+	} else {
+		panic("Invalid roll")
+	}
 }
