dbd93f94677088a107ca6289b408de689a049dce
Ben Miller <ben@beepmill.com>
2018-08-12 23:17:59 -0600

    add tests, clean up parsing

--- dice/dice.go
+++ dice/dice.go
@@ -13,37 +13,63 @@ var (
 	roll string
 )
 
-// Roll TODO
-func Roll(command string) error {
-	rand.Seed(time.Now().UTC().UnixNano())
-	rollValid := regexp.MustCompile(`(\d+)[dD](\d+)`)
-	match := rollValid.FindStringSubmatch(command)
-	fmt.Printf("Rolling %v (%s rolls of size %s)\n", command, match[1], match[2])
-	result := 0
-	rolls, err := strconv.Atoi(match[1])
-	size, err := strconv.Atoi(match[2])
+var rollPattern = regexp.MustCompile(`(\d+)[dD](\d+)([+-]\d+)?`)
 
-	if err != nil {
-		return err
+func parseRoll(s string) (rolls, size, add int, err error) {
+	if rollPattern.MatchString(s) {
+		match := rollPattern.FindStringSubmatch(s)
+		fmt.Printf("Rolling %v (%s rolls of size %s), then adding %s\n", s, match[1], match[2], match[3])
+		rolls, err = strconv.Atoi(match[1])
+		if err != nil {
+			return
+		}
+		size, err = strconv.Atoi(match[2])
+		if err != nil {
+			return
+		}
+		if len(match) == 4 && match[3] != "" {
+			add, err = strconv.Atoi(match[3])
+			if err != nil {
+				return
+			}
+		}
+	} else {
+		err = fmt.Errorf("Cannot parse command '%v'", s)
 	}
+	return
+}
+
+// Roll TODO
+func Roll(command string, seed int64) (result int, err error) {
+	rand.Seed(seed)
+
+	rolls, size, add, err := parseRoll(command)
 	if err != nil {
-		return err
+		return
 	}
+
 	for r := 0; r < rolls; r++ {
 		roll := rand.Intn(size) + 1
+		fmt.Printf("Rolled %v...\n", roll)
 		result += roll
 	}
-	fmt.Printf("%v", result)
-	return nil
+	if add != 0 {
+		fmt.Printf("Adding %v\n", add)
+		result += add
+	}
+	return
 }
 
 func main() {
 	var roll = flag.String("roll", "1d6", "help message goes here")
 	flag.Parse()
 
-	rollValid := regexp.MustCompile(`\d+[dD]\d+`)
-	if rollValid.MatchString(*roll) == true {
-		Roll(*roll)
+	if rollPattern.MatchString(*roll) == true {
+		result, err := Roll(*roll, time.Now().UTC().UnixNano())
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("...%v", result)
 	} else {
 		panic("Invalid roll")
 	}
--- dice/dice_test.go
+++ dice/dice_test.go
@@ -1,18 +1,111 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestRoll(t *testing.T) {
-	testCases := []struct {
-		desc string
+func Test_parseRoll(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantRolls int
+		wantSize  int
+		wantAdd   int
+		wantErr   bool
 	}{
 		{
-			desc: "",
+			"Parses a simple command",
+			args{
+				"1d6",
+			},
+			1,
+			6,
+			0,
+			false,
+		},
+		{
+			"Parses a command with addition",
+			args{
+				"3d12+123",
+			},
+			3,
+			12,
+			123,
+			false,
 		},
 	}
-	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotRolls, gotSize, gotAdd, err := parseRoll(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseRoll() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotRolls != tt.wantRolls {
+				t.Errorf("parseRoll() gotRolls = %v, want %v", gotRolls, tt.wantRolls)
+			}
+			if gotSize != tt.wantSize {
+				t.Errorf("parseRoll() gotSize = %v, want %v", gotSize, tt.wantSize)
+			}
+			if gotAdd != tt.wantAdd {
+				t.Errorf("parseRoll() gotAdd = %v, want %v", gotAdd, tt.wantAdd)
+			}
+		})
+	}
+}
 
+func TestRoll(t *testing.T) {
+	type args struct {
+		command string
+		seed    int64
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantResult int
+		wantErr    bool
+	}{
+		{
+			"it rolls...",
+			args{
+				"1d6",
+				1, // Using a static seed
+			},
+			6,
+			false,
+		},
+		{
+			"it rolls...",
+			args{
+				"10d10+10",
+				1, // Using a static seed
+			},
+			64,
+			false,
+		},
+		{
+			"it requires a parsable command...",
+			args{
+				"1dork6",
+				1, // Using a static seed
+			},
+			0,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotResult, err := Roll(tt.args.command, tt.args.seed)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Roll() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotResult != tt.wantResult {
+				t.Errorf("Roll() = %v, want %v", gotResult, tt.wantResult)
+			}
 		})
 	}
 }
