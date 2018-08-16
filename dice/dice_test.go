ff429d45e4cda550579d237120a366d4b74af4eb
Ben Miller <benmiller@google.com>
2018-08-16 10:00:48 -0600

    tests work again!

--- dice/dice_test.go
+++ dice/dice_test.go
@@ -5,96 +5,110 @@ import (
 	"testing"
 )
 
-func Test_parseCommand(t *testing.T) {
-	type args struct {
-		str string
+func Test_rollSet_parseCommand(t *testing.T) {
+	type fields struct {
+		Command string
+		Rolls   int
+		Size    int
+		Bonus   int
+		Keep    int
 	}
 	tests := []struct {
 		name    string
-		args    args
+		fields  fields
 		wantRs  rollSet
 		wantErr bool
 	}{
 		{
 			"Parses a simple command",
-			args{
-				"1d6",
-			},
-			rollSet{1, 6, 0, 0},
+			fields{Command: "1d6"},
+			rollSet{"1d6", 1, 6, 0, 0},
 			false,
 		},
 		{
 			"Fails to parse an invalid command",
-			args{
-				"1dork6",
-			},
-			rollSet{0, 0, 0, 0},
+			fields{Command: "1dork6"},
+			rollSet{"1dork6", 0, 0, 0, 0},
 			true,
 		},
 		{
 			"Parses a command with addition",
-			args{
-				"3d12+123",
-			},
-			rollSet{3, 12, 123, 0},
+			fields{Command: "3d12+123"},
+			rollSet{"3d12+123", 3, 12, 123, 0},
 			false,
 		},
 		{
 			"Parses a command with keep",
-			args{
-				"3d12+123k2",
-			},
-			rollSet{3, 12, 123, 2},
+			fields{Command: "3d12+123k2"},
+			rollSet{"3d12+123k2", 3, 12, 123, 2},
 			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotRs, err := parseCommand(tt.args.str)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("parseCommand() error = %v, wantErr %v", err, tt.wantErr)
-				return
+			rs := &rollSet{
+				Command: tt.fields.Command,
+				Rolls:   tt.fields.Rolls,
+				Size:    tt.fields.Size,
+				Bonus:   tt.fields.Bonus,
+				Keep:    tt.fields.Keep,
 			}
-			if !reflect.DeepEqual(gotRs, tt.wantRs) {
-				t.Errorf("parseCommand() = %v, want %v", gotRs, tt.wantRs)
+			if err := rs.parseCommand(); (err != nil) != tt.wantErr {
+				t.Errorf("rollSet.parseCommand() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(*rs, tt.wantRs) {
+				t.Errorf("parseCommand() = %v, want %v", *rs, tt.wantRs)
+			}
+			if err := rs.parseCommand(); (err != nil) != tt.wantErr {
+				t.Errorf("rollSet.parseCommand() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
 }
 
 func Test_rollSet_roll(t *testing.T) {
+	type fields struct {
+		Command string
+		Rolls   int
+		Size    int
+		Bonus   int
+		Keep    int
+	}
 	type args struct {
 		seed int64
 	}
 	tests := []struct {
 		name       string
-		rs         *rollSet
+		fields     fields
 		args       args
 		wantResult int
 		wantErr    bool
 	}{
 		{
 			"it rolls...",
-			&rollSet{1, 6, 0, 0},
-			args{
-				1, // Using a static seed
-			},
+			fields{"1d6", 1, 6, 0, 0},
+			args{1},
 			6,
 			false,
 		},
 		{
 			"it rolls...",
-			&rollSet{10, 10, 10, 0},
-			args{
-				1, // Using a static seed
-			},
+			fields{"10d10+10", 10, 10, 10, 0},
+			args{1},
 			64,
 			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotResult, err := tt.rs.roll(tt.args.seed)
+			rs := &rollSet{
+				Command: tt.fields.Command,
+				Rolls:   tt.fields.Rolls,
+				Size:    tt.fields.Size,
+				Bonus:   tt.fields.Bonus,
+				Keep:    tt.fields.Keep,
+			}
+			gotResult, err := rs.roll(tt.args.seed)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("rollSet.roll() error = %v, wantErr %v", err, tt.wantErr)
 				return
